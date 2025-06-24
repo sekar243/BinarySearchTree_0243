b87409e80b8d9b34488df5355eb7c03edaa04e34
sekar243 <kinasekin6602@gmail.com>
2025-06-24 10:40:35 +0700

    menambahkan step 2,3,4

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,7 +35,17 @@ public:
         // step 1: Allocate memory for the new node
         Node * newNode = new Node;
 
-        
+        // step 2: assign value to the data field of the new node
+        newNode->info = x;
 
-    }
+        // step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // step 4: locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currenNode = nullptr;
+        search(x, parent, currenNode);
+
+
 };
