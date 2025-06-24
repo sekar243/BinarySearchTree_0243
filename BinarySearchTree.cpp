a34f1d492811291cd541d9fc81583debde4241e8
sekar243 <kinasekin6602@gmail.com>
2025-06-24 10:47:02 +0700

    menambahkan fungsi inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -93,5 +93,19 @@ public:
         }
     }
 
+    void inorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        inorder(ptr->leftchild);
+        cout << ptr->info <<" ";
+        inorder(ptr->rightchild);
+    }
 
 };
